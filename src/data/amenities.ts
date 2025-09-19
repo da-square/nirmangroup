22bced4e016a9b20e3947aa72f3b3c5d0ef4d39e
da-square <DaSquare12>
2025-09-20 01:04:46 +0530

    develop amenities

---
+++ src/data/amenities.ts
@@ -0,0 +1,56 @@
+export const amenities = [
+  {
+    title: "Exquisite Club House",
+    description:
+      "No more leaving home to take a little vacation! Break the monotony and unwind by indulging in leisure activities with your family in Club Epicuria. From swimming pool, gymnasium, kids play area to banquet hall, the clubhouse has something for everyone.",
+    image: "/images/amenities/Exquisite Club House.png",
+  },
+  {
+    title: "Sprawling Swimming Pool",
+    description:
+      "Make the most of the sunny days and take a dip in our immaculate swimming pool. One of the perfect ways to spend evenings lounging by the cool waters or throwing a pool party you have been planning for long!",
+    image: "/images/amenities/Sprawling Swimming Pool.png",
+  },
+  {
+    title: "Well-equipped Gymnasium",
+    description:
+      "Residents can now lead a healthy lifestyle and achieve fitness goals with ease. There is a state-of-the-art gymnasium within the development, comprising of a wide range of latest equipment to sweat out, build muscles, tone up and get super-fit!",
+    image: "/images/amenities/Well-equipped Gymnasium.png",
+  },
+  {
+    title: "Widespread Lush Green Parks",
+    description:
+      "Take a leisurely stroll and enjoy the heaven full of greenery. A true delight for all the nature lovers and extremely pleasant to look at offering absolute solace of mind and soul. Something that will take your mind off things instantly!",
+    image: "/images/amenities/Widespread lush green parks.png",
+  },
+  {
+    title: "Kids Play Area",
+    description:
+      "Dedicated kids play area for the young ones, packed with fun activities where they can play games and feel free to be completely themselves. The best part? Parents can kick back while kids play and stay occupied.",
+    image: "/images/amenities/Kids Play Area.png",
+  },
+  {
+    title: "Jogging Area Amidst Greenery",
+    description:
+      "Stay fit, go for running every morning or just an evening walk with your loved one on a jogging track that’s nestled in greenery. A much-needed respite from the stresses of everyday life to freshen up and be at one with nature.",
+    image: "/images/amenities/Jogging area amidst greenery.png",
+  },
+  {
+    title: "Temple",
+    description:
+      "A temple is a sacred place for worship and spiritual practices, typically dedicated to deities or religious figures. Within neighborhoods, temples foster community gatherings, celebrations, and rituals.",
+    image: "/images/amenities/Temple.png",
+  },
+  {
+    title: "Easy car parking",
+    description:
+      "Drive into your own, exclusive parking spot and enjoy the comfort of a hassle-free life at Affinity Greens. We offer ample parking space adjacent to every apartment for the car as well as two-wheelers.",
+    image: "/images/amenities/Easy car parking.png",
+  },
+  {
+    title: "Amphitheatre for Meditation and Yoga",
+    description:
+      "Immerse yourself in meditation and yoga to de-stress after a long day in our Alfresco Amphitheatre. The space has been specially designed for people of all age groups looking to improve physical as well as mental wellbeing.",
+    image: "/images/amenities/Amphitheatre for Meditation and Yoga.png",
+  },
+];
